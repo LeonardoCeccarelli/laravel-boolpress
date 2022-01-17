44d94c760942542b87a9a45c858bcb513a11af75
LeonardoCeccarelli <leo.ceccarelli@tiscali.it>
2022-01-17 15:30:06 +0100

    create new compinent vue and import in vue.js

---
+++ resources/js/components/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>{{ welcomeMsg }}</h1>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      welcomeMsg: "Benvenuto Guest",
+    };
+  },
+};
+</script>
