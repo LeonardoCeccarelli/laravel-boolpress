b00dffb6086680c5e22d5b520f9ab038ce5a4d2b
LeonardoCeccarelli <leo.ceccarelli@tiscali.it>
2022-01-18 14:53:21 +0100

    modify style and add dropdown menu posts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -51,6 +51,22 @@
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    Posts
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="#">
+                                        Visualizza Tutti
+                                    </a>
+
+                                    <a class="dropdown-item" href="#">
+                                        Crea Nuovo
+                                    </a>
+                                </div>
+                            </li>
+
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
                                 </a>
 
