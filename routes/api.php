c54eef503bdf6fbe5ee5e2e73b4d8f7a7c7e767f
LeonardoCeccarelli <leo.ceccarelli@tiscali.it>
2022-01-20 13:14:31 +0100

    create folder api in controller and insert postController

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get("/posts", "PostController@getAll");
+Route::get("/posts", "Api\PostController@getAll");
