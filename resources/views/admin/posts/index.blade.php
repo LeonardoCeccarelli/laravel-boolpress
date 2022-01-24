3cb6b085cc7f8d58c6e97d0137d3ddde99ad5a09
LeonardoCeccarelli <leo.ceccarelli@tiscali.it>
2022-01-24 15:23:22 +0100

    fix element

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -1,48 +1,30 @@
 @extends('layouts.app')
 
 @section('content')
-    @if (count($posts) === 0)
-        <h2 class="text-center">Nessun risultato</h2>
-    @else
-        <div class="container">
-            <div class="row row-cols-2">
-                @foreach ($posts as $post)
-                    <div class="col py-3">
-                        <div class="card h-100 position-relative pb-5">
-                            <img src="{{ $post->coverImg }}" class="card-img-top w-25" alt="post_image">
-                            <div class="card-body mb-3">
-                              <h5 class="card-title text-center text-danger text-uppercase mb-2">{{ $post->title }}</h5>
-
-                                @if ($post->subtitle)
-                                    <h5 class="card-title text-center mb-5">{{ $post->subtitle }}</h2>
-                                @endif
-
-                                <p class="card-text text-secondary">{{ mb_strimwidth(strip_tags($post->content), 0, 100, "...") }}</p>
-
-                                <p class="card-text mb-2">Categoria: <span class="text-success">{{ $post->category->name }}</span></p>
-                            
-                                @if ($post->tags->count())
-                                <div class="mb-5">
-                                    @foreach ($post->tags as $tag)
-                                    <h6 class="d-inline-block"><span class="badge badge-secondary">{{ $tag->name }}</span></h6>
-                                    @endforeach
-                                </div>
-                                @endif
-
-                                <div class="d-flex justify-content-between align-items-center info_section">
-                                    <a href="{{ route("admin.posts.show", $post->id) }}" class="btn btn-sm btn-primary">Info</a>
-                                    <p>
-                                        <small>{{ $post->user->name }}</small>
-                                        <br>
-                                        <small>{{ $post->created_at }}</small>
-                                    </p>
-                                </div>
-
-                            </div>
-                        </div>
+@if (count($posts) === 0)
+<h2 class="text-center">Nessun risultato</h2>
+@else
+<div class="container">
+    <div class="row row-cols-1">
+        @foreach ($posts as $post)
+        <div class="col py-3">
+            <div class="card">
+                <div class="card-body d-flex justify-content-between">
+                    <div>
+                        <h4>{{ mb_strimwidth($post->title, 0, 50, "...") }}</h4>
+                        <h6 class="mb-0">
+                            {{ $post->user->name }} <small class="text-secondary">- {{ $post->created_at }}</small>
+                        </h6>
                     </div>
-                @endforeach
+                    <div>
+                        <a href="{{ route('admin.posts.show', $post->id) }}"
+                            class="btn btn-sm btn-primary btn_hover">Info</a>
+                    </div>
+                </div>
             </div>
         </div>
-    @endif
+        @endforeach
+    </div>
+</div>
+@endif
 @endsection
--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -1,42 +1,54 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <h1>Titolo: {{ $post->title }}</h1>
-
+<div class="container">
+    {{-- <div>
+        <h1 class="admin_show_item">
+            <small class="text-secondary mr-5">Titolo:</small>
+            {{ $post->title }}
+        </h1>
         @if ($post->subtitle)
-            <h2>Sottotitolo: {{ $post->subtitle }}</h2>
+        <h2 class="admin_show_item">
+            <small class="text-secondary mr-5">Sottotitolo:</small>
+            {{ $post->subtitle }}
+        </h2>
         @endif
+    </div> --}}
+    {{-- <p>&leftarrow;</p> --}}
+    <h1>Titolo: {{ $post->title }}</h1>
 
-        <p>Contenuto: <br>
-            {!! $post->content !!}
-        </p>
+    @if ($post->subtitle)
+    <h2>Sottotitolo: {{ $post->subtitle }}</h2>
+    @endif
 
-        <h4>Categoria: {{ $post->category->name }}</h4>
+    <p>Contenuto: <br>
+        {!! $post->content !!}
+    </p>
 
-        @if ($post->tags->count())
-            <h4>Tags:
-                @foreach ($post->tags as $tag)
-                <h6 class="d-inline-block"><span class="badge badge-secondary">{{ $tag->name }}</span></h6>
-                @endforeach
-            </h4>
-        @endif
-       
-            
+    <h4>Categoria: {{ $post->category->name }}</h4>
 
-        <h4 class="mb-4">Autore: {{ $post->user->name }}</h4>
-        
-        <div class="d-flex">
-            <a href="{{ route('admin.posts.edit', $post->id) }}" class="btn btn-success">Modifica</a>
+    @if ($post->tags->count())
+    <h4>Tags:
+        @foreach ($post->tags as $tag)
+        <h6 class="d-inline-block"><span class="badge badge-secondary">{{ $tag->name }}</span></h6>
+        @endforeach
+    </h4>
+    @endif
 
-            <form action="{{ route('admin.posts.destroy', $post->id) }}"
-                method="post" class="ml-3">
 
-                @csrf
-                @method("delete")
 
-                <button type="submit" class="btn btn-danger">Elimina</button>
-            </form>
-        </div>
+    <h4 class="mb-4">Autore: {{ $post->user->name }}</h4>
+
+    <div class="d-flex">
+        <a href="{{ route('admin.posts.edit', $post->id) }}" class="btn btn-success">Modifica</a>
+
+        <form action="{{ route('admin.posts.destroy', $post->id) }}" method="post" class="ml-3">
+
+            @csrf
+            @method("delete")
+
+            <button type="submit" class="btn btn-danger">Elimina</button>
+        </form>
     </div>
+</div>
 @endsection
